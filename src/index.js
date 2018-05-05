fd13f6fd62e5e89cafd2308d25311feead8ab943
Fintan Boyle <fintanboyle@gmail.com>
2018-05-05 11:50:56 +0100

    initial webpack app structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../css/style.css';
+import Logo from './logo.svg';
+
+var logo = new Image();
+logo.src = Logo;
+document.body.appendChild(logo);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Yet another fantastic js app'
+        })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },    
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+   }
+};
